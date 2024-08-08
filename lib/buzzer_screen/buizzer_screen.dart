231861964c8665d25047ccd9c965854ac2b73ddb
Bob Taylor <btaylor@allytechllc.com>
2024-08-08 14:42:32 -0400

    first commit

---
+++ lib/buzzer_screen/buizzer_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BuzzerScreen extends StatefulWidget {
+  const BuzzerScreen({super.key});
+
+  @override
+  State<BuzzerScreen> createState() => _BuzzerScreenState();
+}
+
+class _BuzzerScreenState extends State<BuzzerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//TODO: Import the buzzer_service.dart file
+//TODO: Create an instance of the BuzzerService class
+//TODO: Call the initGpio16Output method in the initState method
+//TODO: Call the disposeGpio method in the dispose method
+//TODO: Create the getElevatedButtonBuzzerOn method
+//TODO: Create the getElevatedButtonBuzzerOff method
+//TODO: Create a Scaffold widget
+//TODO: Add an AppBar widget
+//TODO: Add a Center widget
+//TODO: Add a Column widget
+//TODO: Add the getElevatedButtonBuzzerOn method
+//TODO: Add the getElevatedButtonBuzzerOff method
